1e41c4e8546448133d706cb9b1ed45232bef9580
anvarturakulov <anvarsam@mail.ru>
2021-02-19 09:00:57 +0500

    structure and load list

---
+++ src/components/post-list/post-list.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './post-list.css';
+
+import PostListItem from '../post-list-item';
+
+const PostList = ({posts}) => {
+
+    const elements = posts.map((item) => {
+        return (
+            <li key = {item.id} className='list-group-item'>
+                <PostListItem {...item}/>
+            </li>
+        )
+    })
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    )
+}
+
+export default PostList;
